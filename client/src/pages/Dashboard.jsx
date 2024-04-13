b0db0909c4fb628921c8e7428f18f6ca6eadd986
boni437 <166865313+boni437@users.noreply.github.com>
2024-04-14 00:07:26 +0300

    Login is Ready

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,9 @@
 
-
 function Dashboard() {
   return (
-    <div>Dashboard </div>
+    <div className='max-w-screen-xl mx-auto mt-24 mb-64 px-6'>
+    <Link to={"/create"} className=' bg-green-500 p-2 mb-4 inline-block rounded-lg font-semibold'>Create Post</Link>
+    </div>
   )
 }
 
