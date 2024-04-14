aaa721dae2ee89aa78622c8bb9e0400372aeab92
boni437 <166865313+boni437@users.noreply.github.com>
2024-04-14 18:22:21 +0300

    EditPost is ready

--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -2,8 +2,40 @@ import React from 'react'
 
 function EditPost() {
   return (
-    <div>EditPost </div>
-  )
+    <div className="mt-32 mb-32 w-1/2 mx-auto">
+      <form onSubmit="">
+        <input
+          className="w-full my-4 py-2 px-3 border-[1px] outline-none "
+          type="text"
+          // id="title"
+          placeholder="Title"
+        />
+        <input
+          className="w-full my-4 py-2 px-3 border-[1px] outline-none "
+          type="text"
+          placeholder="Author"
+        />
+
+        <textarea
+          className="w-full my-4 py-2 px-3 border-[1px] outline-none min-h-44"
+          type="text"
+          placeholder="description"
+        />
+
+        <input
+          type="file"
+          accept="image/*"
+          // value={file}
+          className=" w-full mt-5 py-2 px-2 border-[1px] outline-none"
+        />
+
+        <button
+          className="w-full my-4 py-2 px-3 border-[1px] bg-green-300 outline-none"
+          type="submit"
+        ></button>
+      </form>
+    </div>
+  );
 }
 
 export default EditPost
